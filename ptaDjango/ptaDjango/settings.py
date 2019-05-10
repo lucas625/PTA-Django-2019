108dc73c101a96f99a34b1e917ed3e97b795038d
vicpantoja <vicpantojadoamaral@gmail.com>
2019-05-10 20:53:09 -0300

    Create first model

--- ptaDjango/ptaDjango/settings.py
+++ ptaDjango/ptaDjango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '07e8pt(*3bf!0td2r2cirhap#q$wc%n%r$(l#^6(raxp#s5(k('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
